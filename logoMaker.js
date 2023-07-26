c926d0650a0090518e6050c5cd0b61bfe803cee7
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-25 20:10:08 -0600

    added code for logo generation options

--- logoMaker.js
+++ logoMaker.js
@@ -1,16 +1,45 @@
-// The logic for the logo maker lives here.
+// The logic for the logo generation lives here.
 
 const fs = require('fs');
 const { prompt } = require('inquirer');
 const shapeOptions = require('./shapes');
 const colorOptions = require('./colors');
-const logoTemplate = require('./templates/logoTemplate.svg');
-const { async } = require('rxjs');
+const logoTemplate = fs.readFileSync('./templates/logoTemplate.svg', 'utf-8');
 
 async function generateLogo() {
-
+  // Rest of the code...
+  const response = await prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for the logo:',
+        choices: shapeOptions
+    },
+    {
+        type: 'list',
+        name: 'color',
+        message: 'Choose a color for the logo:',
+        choices: colorOptions
+    },
+    {
+        type: 'input',
+        name: 'letters',
+        message: 'Enter three letters for the logo:',
+        validate: input => {
+          if (/^[A-Za-z]{3}$/.test(input)) {
+            return true;
+          }
+          return 'Please enter exactly three letters (A-Z or a-z).';
+        }
+    }
+    ]);
 }
 
 module.exports = {
-    generateLogo
-  };
+  generateLogo
+};
